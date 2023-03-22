c6e33d3517966d29d5266145fa903c318315d673
Baturpcu <batusalkim11@gmail.com>
2023-03-22 04:00:49 +0300

    update
    update

---
+++ Projects/proje1/denemecalismalari/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace denemecalismalari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x= 2.2;
+
+            var y = x * 5;
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
