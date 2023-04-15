5f381b031e3349c9b3c978a373bca7497ef0a6c7
Baturpcu <batusalkim11@gmail.com>
2023-04-15 22:45:44 +0300

    s
    s

--- algortimexample/Algoritma example/Algoritma example/Program.cs
+++ algortimexample/Algoritma example/Algoritma example/Program.cs
@@ -4,9 +4,61 @@ namespace Algoritma_example
 {
     class Program
     {
+        enum Islemler
+        {
+            Toplanma=1,
+            Cikarma=2,
+            Carpma=3,
+            Bolme=4
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("heeloo");
+            int A = 100;
+            int B = 20;
+            Islemler secim = (Islemler) (new Random().Next(1,4));
+            switch (secim)
+            {
+                case Islemler.Toplanma:
+                    Console.WriteLine($"{A} + {B} = {A + B}");
+                    break;
+                case Islemler.Cikarma:
+                    Console.WriteLine($"{A} - {B} = {A-B}");
+                    break;
+                case Islemler.Carpma:
+                    Console.WriteLine($"{A} * {B} = {A*B}");
+                    break;
+                case Islemler.Bolme:
+                    Console.WriteLine($"{A} / {B}  ={A/B}");
+                    break;
+                default:
+                    Console.WriteLine("\aGeçersiz İslem!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+        private static void DegerOkuma()
+        {
+            var k = (char)Console.Read();
+
+            if (char.IsDigit(k))
+            {
+                Console.WriteLine("Rakamdır!!'");
+            }
+            else if (char.IsLower(k))
+            {
+                Console.WriteLine("kucuk karakter.");
+            }
+            else if (char.IsUpper(k))
+            {
+                Console.WriteLine("buyuk karakter");
+            }
+            else
+            {
+                Console.WriteLine("bilinmiyor..");
+            }
+            Console.WriteLine(k);
+            Console.ReadKey();
         }
         private static void MutlakDegerOrnegi()
         {
