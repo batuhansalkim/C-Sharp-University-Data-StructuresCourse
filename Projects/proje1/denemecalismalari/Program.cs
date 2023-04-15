940f5e13d5422aa0ba2b0c8bd6e66f071511fe3f
Baturpcu <batusalkim11@gmail.com>
2023-04-15 21:53:04 +0300

    update
    update

--- Projects/proje1/denemecalismalari/Program.cs
+++ Projects/proje1/denemecalismalari/Program.cs
@@ -7,8 +7,10 @@ namespace denemecalismalari
         static void Main(string[] args)
         {
             var x= 2.2;
-
+            var z = 6;
+            var batu = x+z; 
             var y = x * 5;
+            Console.WriteLine(batu);
             Console.WriteLine(y);
             Console.ReadKey();
         }
---
+++ algortimexample/Algoritma example/Algoritma example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Algoritma_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("heeloo");
+        }
+        private static void MutlakDegerOrnegi()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.WriteLine($"|{n}| = {n * -1}");
+            }
+            else
+            {
+                Console.WriteLine($"|{n}| = {n}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
